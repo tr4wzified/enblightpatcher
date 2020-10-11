41bd06ab25dedff6499783964fc66298212cd1dd
tr4wzified <55751269+tr4wzified@users.noreply.github.com>
2020-10-11 12:54:21 +0200

    Upgrade Mutagen + Synthesis, add worldspace patching functionality

--- ENBLightPatcher/Program.cs
+++ ENBLightPatcher/Program.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using Mutagen.Bethesda;
 using Mutagen.Bethesda.Synthesis;
 using Mutagen.Bethesda.Skyrim;
-using Newtonsoft.Json.Serialization;
 using Wabbajack.Common;
+using Noggog;
 
 namespace ENBLightPatcher
 {
@@ -24,77 +24,53 @@ namespace ENBLightPatcher
             return SynthesisPipeline.Instance.Patch<ISkyrimMod, ISkyrimModGetter>(
                 args: args,
                 patcher: RunPatch,
-                new UserPreferences()
+                new UserPreferences
                 {
-                    ActionsForEmptyArgs = new RunDefaultPatcher()
+                    ActionsForEmptyArgs = new RunDefaultPatcher
                     {
                         IdentifyingModKey = "ENBLightPatcher.esp",
                         TargetRelease = GameRelease.SkyrimSE
                     }
-                }
-            );
+                });
         }
 
         public static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
         {
-            FormKey[] cells = { };
 
-            foreach (var mod in state.LoadOrder.PriorityOrder)
+            // Part 1 - patch every light record in CELL records
+            /*
+            foreach(var cellRecord in state.LoadOrder.PriorityOrder.WinningOverrides<ICellGetter>())
             {
-                if (mod.Mod == null) continue;
-
-                foreach (var worldspaceGetter in mod.Mod.Worldspaces)
+            }
+            */
+            // Part 2 - patch every light record in WRLD records
+            foreach (var worldspaceGetter in state.LoadOrder.PriorityOrder.WinningOverrides<IWorldspaceGetter>())
+            {
+                var wCopy = worldspaceGetter.DeepCopy();
+                bool worldspaceUpdated = false;
+                foreach (var blockGetter in wCopy.SubCells)
                 {
-                    foreach (var blockGetter in worldspaceGetter.SubCells)
+                    foreach (var subBlockGetter in blockGetter.Items)
                     {
-                        foreach (var subBlockGetter in blockGetter.Items)
+                        foreach (var cell in subBlockGetter.Items)
                         {
-                            foreach (var cell in subBlockGetter.Items)
+                            foreach (var refr in cell.Persistent)
                             {
-                                foreach (var refr in cell.Persistent)
-                                {
-                                    switch (refr)
-                                    {
-                                        case IPlacedObject placed:
-                                            if (placed.Base.TryResolve.try
-                                            {
-
-                                            }
-                                            break;
-
-                                        default: continue;
-                                    }
-                                }
+                                if (!(refr is IPlacedObject)) continue;
+                                IPlacedObject placedObject = (IPlacedObject)refr;
+                                if (placedObject.LightData == null) continue;
+                                placedObject.LightData.FadeOffset = placedObject.LightData.FadeOffset / 2;
+                                worldspaceUpdated = true;
                             }
                         }
                     }
                 }
-
-                //    foreach (var refr in cell.Persistent)
-                //    {
-                //        switch (refr)
-                //        {
-                //            case IPlacedObjectGetter placed:
-                //                placed.Base.
-                //                break;
-
-                //            default:
-                //                continue;
-                //        }
-
-                //        if (refr. == null) continue;
-
-                //        if (!refr.Name.ContainsInsensitive("LIGH:")) continue;
-
-                //        if (!refr.Name.ContainsInsensitive("Torch") && !refr.Name.ContainsInsensitive("Camp") && !refr.Name.ContainsInsensitive("Candle")) continue;
-
-                //    }
+                if (worldspaceUpdated) state.PatchMod.Worldspaces.GetOrAddAsOverride(wCopy);
             }
-
+            // Part 2 - Patch every LIGH record
             foreach (var light in state.LoadOrder.PriorityOrder.WinningOverrides<ILightGetter>())
             {
                 if (light.EditorID == null) continue;
-
                 if (!light.EditorID.ContainsInsensitive("Torch") && !light.EditorID.ContainsInsensitive("Camp") && !light.EditorID.ContainsInsensitive("Candle")) continue;
 
                 var modifiedLight = state.PatchMod.Lights.GetOrAddAsOverride(light);
